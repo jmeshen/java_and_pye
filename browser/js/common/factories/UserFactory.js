fc03dc2f6d6ce03a283f005ef3bdd1dea547876a
Jamie <jmeshen@gmail.com>
2015-08-09 05:46:06 -0400

    MOARRRRRRRRRR

--- browser/js/common/factories/UserFactory.js
+++ browser/js/common/factories/UserFactory.js
@@ -1,5 +1,6 @@
 app.factory('UserFactory', function ($http) {
   var match;
+  var moar;
   return {
     getUser: function(id) {
       return $http.get('/api/users/'+id).then(function(response) {
@@ -12,7 +13,9 @@ app.factory('UserFactory', function ($http) {
       return $http.put('/api/users/filtermatches/'+id).then(function(response) {
         console.log('response from factory', response.data)
         match = response.data[0];
+        moar = response.data.slice(1);
         console.log('match from filterMatches ', match)
+        console.log('moarrrrrrr ', moar)
         return response.data;
       }, function(err) {
         return new Error(err.message);
@@ -20,6 +23,9 @@ app.factory('UserFactory', function ($http) {
     },
     getMatch: function() {
       return match;
+    },
+    getMoar: function() {
+      return moar;
     }
   }
 
